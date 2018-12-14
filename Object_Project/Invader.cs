272fff2f412a631e11f172c8b8d62cb2c6288927
Lewis, Clayton <Clayton.Lewis@geappliances.com>
2018-12-14 14:36:33 -0500

    Getters and Setters

--- Object_Project/Invader.cs
+++ Object_Project/Invader.cs
@@ -3,5 +3,16 @@ namespace TreehouseDefense {
 
     class Invader {
 
+        private MapLocation _location;
+
+        public MapLocation Location {
+            get {
+                return _location;
+            }
+            set {
+                _location = value;
+            }
+        }
+
     }
 }
