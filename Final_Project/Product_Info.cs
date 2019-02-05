7f8874df7bcccc021125d5e456162cc979f3d00b
Lewis, Clayton <Clayton.Lewis@geappliances.com>
2019-02-05 09:29:51 -0500

    Commit

---
+++ Final_Project/Product_Info.cs
@@ -0,0 +1,12 @@
+
+namespace Code_Louisville{
+
+    class Product_Info{
+
+        string Product_ID;
+        string Product_Name;
+        double Price;
+        string Prodcut_Description;
+        
+    }
+}
