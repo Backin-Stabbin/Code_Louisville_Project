4dde9c9f25de261ac7834c9ca8085d2e00ed1e35
Lewis, Clayton <Clayton.Lewis@geappliances.com>
2019-02-18 18:42:09 -0500

    Commit

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -1,11 +1,26 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace Treehouse.CodeChallenges {
-    public abstract class SequenceDetector {
-        public virtual string Description => "";
 
-        public int[] LastScannedSequence { get; protected set; }
+    public class Program {
 
-        public abstract bool Scan (int[] sequence);
+        public static void Main () {
+
+        }
+
+        public static WeatherForecast ParseWeatherForecast (string[] values) {
+            WeatherForecast weatherForecast = new WeatherForecast ();
+            weatherForecast.WeatherStationId = values[0];
+            DateTime timeOfDay;
+            if (DateTime.TryParse (values[1], out timeOfDay)) {
+                weatherForecast.TimeOfDay = timeOfDay;
+            }
+
+            return weatherForecast;
+        }
+
     }
+
 }
--- Soccer_Stats/Program.cs
+++ Soccer_Stats/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace Soccer_Stats {
@@ -7,21 +8,60 @@ namespace Soccer_Stats {
 
         static void Main () {
 
-            string currentdirectory = Directory.GetCurrentDirectory();
-            DirectoryInfo directory = new DirectoryInfo(currentdirectory);
-            var fileName = Path.Combine(directory.FullName, "Data.txt");
-            var file = new FileInfo(fileName);
-            if (file.Exists){
-                using (var reader = new StreamReader(file.FullName)){
-                    Console.SetIn(reader);
-                    Console.WriteLine(Console.ReadLine());
-                }
-            }
-            else {
-                Console.WriteLine("File Missing");
-            }
+            string currentdirectory = Directory.GetCurrentDirectory ();
+            DirectoryInfo directory = new DirectoryInfo (currentdirectory);
+            var fileSoccerGameResultscsv = Path.Combine (directory.FullName, "SoccerGameResults.csv");
+            var fileContents = ReadSoccerResults (fileSoccerGameResultscsv);
 
-            Console.ReadKey();
+            Console.WriteLine (fileContents.Count);
+        }
+
+        public static string ReadFile (string fileName) {
+            using (var reader = new StreamReader (fileName)) {
+                return reader.ReadToEnd ();
+            }
+        }
+
+        public static List<Game_Result> ReadSoccerResults (string fileName) {
+            var soccerResults = new List<Game_Result> ();
+            using (var reader = new StreamReader (fileName)) {
+                string line = "";
+                reader.ReadLine ();
+                while ((line = reader.ReadLine ()) != null) {
+                    var gameResult = new Game_Result ();
+                    string[] values = line.Split (',');
+                    DateTime gameDate;
+                    if (DateTime.TryParse (values[0], out gameDate)) {
+                        gameResult.GameDate = gameDate;
+                    }
+                    gameResult.TeamName = values[1];
+                    HomeOrAway homeOrAway;
+                    if (Enum.TryParse (values[2], out homeOrAway)) {
+                        gameResult.HomeOrAway = homeOrAway;
+                    }
+                    soccerResults.Add (gameResult);
+                    int parseInt;
+                    if (int.TryParse (values[3], out parseInt)) {
+                        gameResult.Goals = parseInt;
+                    }
+                    if (int.TryParse (values[4], out parseInt)) {
+                        gameResult.GoalAttempts = parseInt;
+                    }
+                    if (int.TryParse (values[5], out parseInt)) {
+                        gameResult.ShotsOnGoal = parseInt;
+                    }
+                    if (int.TryParse (values[6], out parseInt)) {
+                        gameResult.ShotsOffGoal = parseInt;
+                    }
+                    double possessionPercent;
+                    if (double.TryParse(values[7], out possessionPercent)){
+                        gameResult.PossessionPercent = possessionPercent;
+                    }
+                    
+                }
+
+            }
+            return soccerResults;
         }
     }
 }
