1c0648314e4bdc9b45d5b7c7e023a2f26da92820
Lewis, Clayton <Clayton.Lewis@geappliances.com>
2019-03-05 10:39:18 -0500

    Added Database and Building Class

---
+++ Code_Louisville_Project/Building.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+
+namespace Code_Louisville {
+
+    public class Building {
+
+        public static List<string> Get_List_Of_Buildings() {
+            var buildingChoices = new List<string> {
+                "BLDG1",
+                "BLDG2",
+                "BLDG3",
+                "BLDG4",
+                "BLDG5"
+            };
+
+            return buildingChoices;
+        }
+
+    }
+}
